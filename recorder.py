3e3aca351390a08e5cc3a568df744ac690c2d00b
Varun Saran <varunsaran@Varuns-MacBook-Pro.local>
2019-10-26 23:13:11 -0700

    made calculate function

--- recorder.py
+++ recorder.py
@@ -8,16 +8,18 @@ client = wolframalpha.Client(appId)
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-with mic as source:
+'''uncomment for mic functionality
+    with mic as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
-
-a = r.recognize_google(audio)
-print(a)
+'''
+#a = r.recognize_google(audio)
+#print(a)
 #a= 'e^x/x^2 = 15'
-res = client.query(a)
-#for pod in res.pods:
-    #print("**********************pod:", pod.keys() )
-#print("**********res:", res)
-#print("**********res.results:", res.results)
-print("**********res.results:", next(res.results).text)
+def calculate(str):
+    res = client.query(str)
+    return next(res.results).text
+
+
+a = 'integrate x squared'
+print(calculate(a))
