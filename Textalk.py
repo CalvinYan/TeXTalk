54445cd956817c64d5d1d73de6fc6d959cedef7c
CalvinYan <calviny524@gmail.com>
2019-10-27 04:50:37 -0700

    Testing changes, update README

--- Textalk.py
+++ Textalk.py
@@ -41,6 +41,19 @@ def convert_fractions(s):
         start_index, end_index = get_subformula(index-1, -1), get_subformula(index+1, 1)
         # print(index, start_index, end_index)
         s = s[:start_index] + '\\frac{' + s[start_index:index] + '}{' + s[index+1:end_index] + '}' + s[end_index:]
+    print(s)
+    while 'integral' in s:
+        index = s.find('integral')
+        end_index = get_subformula(index+len('integral'), 1)
+        s = s[:index] + '\\int{' + s[index+len('integral'):end_index] + '}' + s[end_index:]
+    while 'root' in s:
+        index = s.find('root')
+        end_index = get_subformula(index+len('root'), 1)
+        s = s[:index] + '\\sqrt{' + s[index+len('root'):end_index] + '}' + s[end_index:]
+    while 'log' in s:
+        index = s.find('log')
+        end_index = get_subformula(index+len('log'), 1)
+        s = s[:index] + '\\ln{' + s[index+len('log'):end_index] + '}' + s[end_index:]
     return s
 
 
@@ -59,23 +72,23 @@ def parsestr_numeric(s):
     return s
 
 def parsestr_substitute(s):
-    for phrase in sorted(words_substituted.keys(), key=lambda k: len(k.split())):
-        #print(phrase, phrase in s)
+    for phrase in sorted(words_substituted.keys(), key=lambda k: len(k.split()) * 10 + (len(k) == 1 and len(k[0]))):
+        # print(phrase, phrase in s)
         s = s.replace(phrase, words_substituted[phrase])
     return s
 
 # Returns a lst of indices containing <quantity and all>
 def quantity(s):
     lst = []
-    while 'the quantity' in s and 'all' in s:
+    while 'the quantity of' in s and 'all' in s:
 
         # Remember to add index by 1 more for end index because second l in all will not be included \
         # in range() if not inclusive
-        start_index, end_index = s.find('the quantity'), s.find('all')
-        s = s[:start_index] + '(' + convert(s[start_index+len('the quantity'):end_index].strip()) + ')' + s[end_index+len('all'):]
-    while 'the quantity' in s:
-        start_index = s.find('the quantity')
-        s = s[:start_index] + '(' + convert(s[start_index+len('the quantity'):].strip()) + ')'
+        start_index, end_index = s.find('the quantity of'), s.find('all')
+        s = s[:start_index] + '(' + convert(s[start_index+len('the quantity of'):end_index].strip()) + ')' + s[end_index+len('all'):]
+    while 'the quantity of' in s:
+        start_index = s.find('the quantity of')
+        s = s[:start_index] + '(' + convert(s[start_index+len('the quantity of'):].strip()) + ')'
     while 'all' in s:
         end_index = s.find('all')
         s = '(' + convert(s[:end_index]) + ')' + s[end_index+len('all'):]
@@ -86,12 +99,13 @@ def quantity(s):
 # print(parsestr('one plus two minus three times four equals zero'))
 # print(parsestr('I just did three hundred and sixty five epsilon delta proofs'))
 # print(parsestr('The integral from zero to x times 5 of x plus x to the power of two'))
-# print(convert(quantity('4 times the quantity x squared over 2 all over 3')))
-# print(convert(quantity('4 times the quantity x squared over 2 all over 3 all over 5')))
-# print(convert(quantity('the quantity 4 times the quantity x squared over 2 all over 3 all over 5')))
-# print(convert_to_proto(quantity('x squared times the quantity the quantity alpha plus beta all over 2 + 5 is less than forty')))
+# print(convert(quantity('4 times the quantity of x squared over 2 all over 3')))
+# print(convert(quantity('4 times the quantity of x squared over 2 all over 3 all over 5')))
+# print(convert(quantity('the quantity of 4 times the quantity of x squared over 2 all over 3 all over 5')))
+# print(convert_to_proto(quantity('x squared times the quantity of the quantity of alpha plus beta all over 2 + 5 is less than forty')))
 # print(convert_fractions('(1/2)/3'))
-# print(convert_to_proto('the quantity x squared plus four over three plus the quantity the quantity x plus six all over five all over the quantity x plus five x cubed all over three x plus x'))
-# print(convert_fractions(convert_to_proto('the quantity x squared plus four over three plus the quantity the quantity x plus six all over five all over the quantity x plus five x cubed all all over three x plus x')))
-# print(convert('the quantity negative b plus b squared minus four a c all over the quantity two a'))
+# print(convert_to_proto('the quantity of x squared plus four over three plus the quantity of the quantity of x plus six all over five all over the quantity of x plus five x cubed all over three x plus x'))
+# print(convert_fractions(convert_to_proto('the quantity of x squared plus four over three plus the quantity of the quantity of x plus six all over five all over the quantity of x plus five x cubed all all over three x plus x')))
+# print(convert('the quantity of negative b plus b squared minus four a c all over the quantity of two a'))
 # print(convert('the integral of x squared equals one over three times x cubed'))
+print(convert('the integral of the quantity of the square root of the quantity of x squared plus x over 2 times natural log 3 dx'))
