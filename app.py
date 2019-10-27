c163d29e996ab11ee8b947faeaf59f2b04e9a5ac
Deoy12 <55270451+Deoy12@users.noreply.github.com>
2019-10-26 23:29:32 -0700

    changed record function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, jsonify
 import speech_recognition as sr
+import Textalk.parsestr as parse
 
 app = Flask(__name__)
 @app.route('/')
@@ -13,5 +14,8 @@ def record():
     with mic as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
-    a = r.recognize_google(audio)
-    return a
+    text = r.recognize_google(audio)
+    converted, calculated = parse(text)
+    return "Text: " + text + "\" +
+            "Converted:" + converted + "\" +
+            "Calculated: " + calculated
