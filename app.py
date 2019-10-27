777f760c0d05ce773141b2ba22212a51d13d4f19
Deoy12 <55270451+Deoy12@users.noreply.github.com>
2019-10-27 03:37:42 -0700

    updated GUI

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, jsonify
 import speech_recognition as sr
-import Textalk.parsestr as parse
+import Textalk
 
 app = Flask(__name__)
 @app.route('/')
@@ -10,12 +10,13 @@ def index():
 @app.route('/forward/', methods = ['POST'])
 def record():
     r = sr.Recognizer()
+    r.pause_threshold = 2.0
     mic = sr.Microphone()
     with mic as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
     text = r.recognize_google(audio)
-    converted, calculated = parse(text)
-    return "Text: " + text + "\" +
-            "Converted:" + converted + "\" +
-            "Calculated: " + calculated
+    converted, calculated = Textalk.parsestr(text)
+    if 'Incorrect' in calculated:
+        return render_template('Display.html', converted = 'Error', calculated = calculated)
+    return render_template('Display.html', converted = converted, calculated = calculated)
