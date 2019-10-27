ec3aef0f2db386a009f90d95c83d8159239d213f
Varun Saran <varunsaran@Varuns-MacBook-Pro.local>
2019-10-26 22:33:59 -0700

    updated recorder.py with Wolfram Alpha API

--- recorder.py
+++ recorder.py
@@ -1,6 +1,11 @@
+import requests
 import speech_recognition as sr
-r = sr.Recognizer()
+import wolframalpha
 
+appId = "E6A599-295PX2RKJE"
+client = wolframalpha.Client(appId)
+
+r = sr.Recognizer()
 mic = sr.Microphone()
 
 with mic as source:
@@ -9,3 +14,10 @@ with mic as source:
 
 a = r.recognize_google(audio)
 print(a)
+#a= 'e^x/x^2 = 15'
+res = client.query(a)
+#for pod in res.pods:
+    #print("**********************pod:", pod.keys() )
+#print("**********res:", res)
+#print("**********res.results:", res.results)
+print("**********res.results:", next(res.results).text)
