c6ec7d3f0338e34c230ad3e0027bab7d87c533d6
Varun Saran <varunsaran@Varuns-MacBook-Pro.local>
2019-10-27 02:01:19 -0700

    added try, catch so it doesn't crash

--- recorder.py
+++ recorder.py
@@ -5,22 +5,28 @@ import wolframalpha
 appId = "E6A599-295PX2RKJE"
 client = wolframalpha.Client(appId)
 
-r = sr.Recognizer()
-r.pause_threshold = 1.5
-mic = sr.Microphone()
 
-'''uncomment for mic functionality
+
+'''uncomment for mic functionality. The following is currently done in app.py
+r = sr.Recognizer()
+r.pause_threshold = 15.0
+mic = sr.Microphone()
     with mic as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
-'''
-#a = r.recognize_google(audio)
+
+a = r.recognize_google(audio)
 #print(a)
-#a= 'e^x/x^2 = 15'
+a= 'e^x/x^2 = 15'
+'''
 def calculate(str):
-    res = client.query(str)
-    return next(res.results).text
+    try:
+        res = client.query(str)
+        return next(res.results).text
+    except:
+        return "Incorrect input. Try again"
 
 
-a = 'integrate x squared'
+
+a = 'Benjamin Franklin'
 print(calculate(a))
