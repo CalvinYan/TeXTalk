c6a62aa084c115e5c2ea3344ff38a55159729359
Varun Saran <varunsaran@Varuns-MacBook-Pro.local>
2019-10-27 04:47:18 -0700

    removed 'ben franklin test'

--- recorder.py
+++ recorder.py
@@ -22,11 +22,6 @@ a= 'e^x/x^2 = 15'
 def calculate(str):
     try:
         res = client.query(str)
-        return next(res.results).text
+        return res
     except:
         return "Incorrect input. Try again"
-
-
-
-a = 'Benjamin Franklin'
-print(calculate(a))
